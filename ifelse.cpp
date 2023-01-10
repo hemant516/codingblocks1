4ed3995cdafb8c930c1c18ccf17995a13aeef522
hemant <hemantsolanki516@gmail.com>
2023-01-10 20:24:40 +0530

    commit message

--- ifelse.cpp
+++ ifelse.cpp
@@ -2,20 +2,32 @@
 
 using namespace std;
 
-int main() {
+int main()
+
+ {
 
 	int marks;
 
 	cout << "enter your marks : ";
 	cin >> marks;
 
-	if(marks >= 91 and marks <= 100) {
+	if(marks >= 91 and marks <= 100) 
+	
+	{
 		cout << "Your grade is A" << endl;
-	} else if(marks >= 81 and marks <= 90) {
+	} else if(marks >= 81 and marks <= 90)
+	
+	
+	 {
 		cout << "Your grade is B" << endl;
-	} else if(marks >= 71 and marks <= 80) {
+	} else if(marks >= 71 and marks <= 80)
+	
+	 {
 		cout << "Your grade is C" << endl;
-	} else {
+	} else
+	
+	
+ {
 		cout << "Your grade is D" << endl;
 	}
 
